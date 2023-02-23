024cbbe448e2d36aae4bc91be9c963c180652c13
Kosta Kazakov <kostakazakoff@gmail.com>
2023-02-23 23:16:34 +0200

    problem solved

---
+++ JS/JS Syntax Fundamentals/Exercise/04.print-and-sum.js
@@ -0,0 +1,10 @@
+function printAndSum(start, end){
+    let sum = 0;
+    let arr = [];
+    for (let i = start; i <= end; i++){
+        arr.push(i);
+        sum += i;
+    }
+    console.log(arr.join(' '))
+    console.log(`Sum: ${sum}`)
+}
