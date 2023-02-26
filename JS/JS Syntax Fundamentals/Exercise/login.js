f25cf32bd89157abbd502a9ade5a447078151f08
Kosta Kazakov <kostakazakoff@gmail.com>
2023-02-26 13:23:19 +0200

    problem solved

---
+++ JS/JS Syntax Fundamentals/Exercise/login.js
@@ -0,0 +1,21 @@
+function login(arr){
+    let username = arr[0];
+    let passwords = arr.slice(1)
+    let counter = 0;
+    for (let i = 0; i < passwords.length; i++){
+        counter += 1;
+        let pwd = passwords[i];
+        pwd = pwd.split('').reverse(pwd).join('');
+        if (username === pwd){
+            console.log(`User ${username} logged in.`);
+            break;
+        } if (counter === 4){
+            console.log(`User ${username} blocked!`);
+            break;
+        } else {
+            console.log('Incorrect password. Try again.');
+        };
+    }
+}
+
+// login(['sunny','rainy','cloudy','sunny','not sunny'])
