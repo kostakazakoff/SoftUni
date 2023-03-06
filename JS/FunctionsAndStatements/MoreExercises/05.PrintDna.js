96bd29a5c74b5129daf6974e1fead68ed6ee6a1c
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-06 15:57:45 +0200

    optimising

--- JS/FunctionsAndStatements/MoreExercises/05.PrintDna.js
+++ JS/FunctionsAndStatements/MoreExercises/05.PrintDna.js
@@ -1,12 +1,14 @@
 function printDna(n) {
     let symbols;
     let line;
-    const dnaSeq = 'ATCGTTAGGG'.match(/[A-Z]{2}/g);
+    let index;
+    const dnaSeq = 'ATCGTTAGGG';
 
     for (let i = 0; i < n; i++) {
         let stars = '';
         let dashes = '';
-        symbols = dnaSeq[i % dnaSeq.length].split('');
+        index = (i * 2 % dnaSeq.length);
+        symbols = dnaSeq.slice(index, index+2).split('');
 
         if (i % 4 === 0) {
             stars = '*'.repeat(2);
@@ -22,4 +24,28 @@ function printDna(n) {
     }
 }
 
-// printDna(10)
+// function printDna(n) {
+//     let symbols;
+//     let line;
+//     const dnaSeq = 'ATCGTTAGGG'.match(/[A-Z]{2}/g);
+
+//     for (let i = 0; i < n; i++) {
+//         let stars = '';
+//         let dashes = '';
+//         symbols = dnaSeq[i % dnaSeq.length].split('');
+
+//         if (i % 4 === 0) {
+//             stars = '*'.repeat(2);
+//         } else if (i % 2 !== 0) {
+//             stars = '*';
+//             dashes = '-'.repeat(2);
+//         } else if (i % 2 === 0 && i % 2 !== 4) {
+//             dashes = '-'.repeat(4);
+//         }
+
+//         line = `${stars}${symbols[0]}${dashes}${symbols[1]}${stars}`;
+//         console.log(line);
+//     }
+// }
+
+printDna(6)
