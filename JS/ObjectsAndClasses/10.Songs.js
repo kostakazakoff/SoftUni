5098948e299a326652783d53592db73411f210dd
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-08 18:07:00 +0200

    optimising

--- JS/ObjectsAndClasses/10.Songs.js
+++ JS/ObjectsAndClasses/10.Songs.js
@@ -5,21 +5,19 @@ function songsFilter(arr) {
             this.name = name;
             this.time = time;
         }
-        isChosen(type) {
-            if (type == this.typeList || type == 'all') {
-                return true;
+        print(chosenType) {
+            if (chosenType == this.typeList || chosenType == 'all') {
+                console.log(this.name);
             };
         };
-    }
+    };
 
     [numberOfSongs, ...songsInfo] = arr;
     choice = songsInfo[songsInfo.length - 1];
     songsInfo.slice(0, -1)
         .forEach(song => {
             newSong = new Song(...song.split('_'));
-            if (newSong.isChosen(choice)) {
-                console.log(newSong.name)
-            }
+            newSong.print(choice)
         });
 }
 
@@ -28,5 +26,5 @@ function songsFilter(arr) {
 //     'listenLater_Andalouse_3:24',
 //     'favourite_In To The Night_3:58',
 //     'favourite_Live It Up_3:48',
-//     'all']
+//     'listenLater']
 // )
