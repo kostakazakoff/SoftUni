b3fa7ed32da25411669303d0f5a7cd5757df4f3f
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-09 18:59:01 +0200

    refactoring

--- JS/ObjectsAndClasses/Exercises/03.StoreProvision.js
+++ JS/ObjectsAndClasses/Exercises/03.StoreProvision.js
@@ -17,10 +17,7 @@ function calcStoreProvision(current, ordered) {
     };
 }
 
-calcStoreProvision([
-    'Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'
-    ],
-    [
-    'Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30'
-    ]
-    )
+// calcStoreProvision(
+//     ['Chips', '5', 'CocaCola', '9', 'Bananas', '14', 'Pasta', '4', 'Beer', '2'],
+//     ['Flour', '44', 'Oil', '12', 'Pasta', '7', 'Tomatoes', '70', 'Bananas', '30']
+//     )
