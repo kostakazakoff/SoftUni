e1041562c7581c918c2715aa21daebfe5ca40343
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-09 10:17:28 +0200

    problem solved

--- JS/ObjectsAndClasses/Exercises/01.Employees.js
+++ JS/ObjectsAndClasses/Exercises/01.Employees.js
@@ -1,3 +1,17 @@
 function employeePersonalNumber(arr) {
-    
+    let book = {};
+    arr.forEach(employee => {
+        book[employee] = employee.length;
+    });
+    for (person in book) {
+        console.log(`Name: ${person} -- Personal Number: ${book[person]}`);
+    }
 }
+
+// employeePersonalNumber([
+//     'Silas Butler',
+//     'Adnaan Buckley',
+//     'Juan Peterson',
+//     'Brendan Villarreal'
+//     ]
+//     )
