1955a954042dc1a08dc2a7bc25027f1f16f7047c
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-10 15:05:14 +0200

    var  2

--- JS/ObjectsAndClasses/05.Inventory.js
+++ JS/ObjectsAndClasses/05.Inventory.js
@@ -1,23 +1,39 @@
+// function Inventory(arr) {
+//     let heroes = [];
+//     arr.forEach(line => {
+//         let [heroName, level, items] = line.split(' / ');
+//         let hero = { heroName, level, items };
+//         heroes.push(hero);
+//     });
+
+//     heroes
+//     .sort((heroA, heroB) => heroA.level - heroB.level)
+//     .forEach(hero => {
+//         console.log(`Hero: ${hero.heroName}`);
+//         console.log(`level => ${hero.level}`);
+//         console.log(`items => ${hero.items}`);
+//     })
+// }
+
 function Inventory(arr) {
-    let heroes = [];
-    arr.forEach(line => {
+    arr.reduce((data, line) => {
         let [heroName, level, items] = line.split(' / ');
         let hero = { heroName, level, items };
-        heroes.push(hero);
-    });
-
-    heroes
-    .sort((heroA, heroB) => heroA.level - heroB.level)
-    .forEach(hero => {
-        console.log(`Hero: ${hero.heroName}`);
-        console.log(`level => ${hero.level}`);
-        console.log(`items => ${hero.items}`);
-    })
+        data.push(hero);
+        return data;
+    }, [])
+        .sort((heroA, heroB) => heroA.level - heroB.level)
+        .forEach(hero => {
+            console.log(`Hero: ${hero.heroName}`);
+            console.log(`level => ${hero.level}`);
+            console.log(`items => ${hero.items}`);
+        })
 }
 
-Inventory([
-    'Isacc / 25 / Apple, GravityGun',
-    'Derek / 12 / BarrelVest, DestructionSword',
-    'Hes / 1 / Desolator, Sentinel, Antara'
-]
-)
+// Inventory(
+//     [
+//     'Isacc / 25 / Apple, GravityGun',
+//     'Derek / 12 / BarrelVest, DestructionSword',
+//     'Hes / 1 / Desolator, Sentinel, Antara'
+//     ]
+// )
