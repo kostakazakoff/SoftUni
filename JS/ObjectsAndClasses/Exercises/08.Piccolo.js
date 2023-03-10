fe758b832d517ae1572591705a8f5f6ec70e3073
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-10 20:43:20 +0200

    problem solved

--- JS/ObjectsAndClasses/Exercises/08.Piccolo.js
+++ JS/ObjectsAndClasses/Exercises/08.Piccolo.js
@@ -21,17 +21,17 @@ function parking(arr) {
 }
 
 
-parking(
-    [
-        'IN, CA2844AA',
-        'IN, CA1234TA',
-        'OUT, CA2844AA',
-        'IN, CA9999TT',
-        'IN, CA2866HI',
-        'OUT, CA1234TA',
-        'IN, CA2844AA',
-        'OUT, CA2866HI',
-        'IN, CA9876HH',
-        'IN, CA2822UU'
-    ]
-)
+// parking(
+//     [
+//         'IN, CA2844AA',
+//         'IN, CA1234TA',
+//         'OUT, CA2844AA',
+//         'IN, CA9999TT',
+//         'IN, CA2866HI',
+//         'OUT, CA1234TA',
+//         'IN, CA2844AA',
+//         'OUT, CA2866HI',
+//         'IN, CA9876HH',
+//         'IN, CA2822UU'
+//     ]
+// )
