7139e993a2d60f11b4d265efed42f37eec5de055
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-11 21:23:49 +0200

    refactoring

--- JS/ObjectsAndClasses/MoreExercises/04.FlightSchedule.js
+++ JS/ObjectsAndClasses/MoreExercises/04.FlightSchedule.js
@@ -39,21 +39,20 @@ function FlightSchedule(matrix) {
 }
 }
 
-
-FlightSchedule([['WN269 Delaware',
-'FL2269 Oregon',
- 'WN498 Las Vegas',
- 'WN3145 Ohio',
- 'WN612 Alabama',
- 'WN4010 New York',
- 'WN1173 California',
- 'DL2120 Texas',
- 'KL5744 Illinois',
- 'WN678 Pennsylvania'],
- ['DL2120 Cancelled',
- 'WN612 Cancelled',
- 'WN1173 Cancelled',
- 'SK330 Cancelled'],
- ['Ready to fly']
-]
-)
+// FlightSchedule([['WN269 Delaware',
+// 'FL2269 Oregon',
+//  'WN498 Las Vegas',
+//  'WN3145 Ohio',
+//  'WN612 Alabama',
+//  'WN4010 New York',
+//  'WN1173 California',
+//  'DL2120 Texas',
+//  'KL5744 Illinois',
+//  'WN678 Pennsylvania'],
+//  ['DL2120 Cancelled',
+//  'WN612 Cancelled',
+//  'WN1173 Cancelled',
+//  'SK330 Cancelled'],
+//  ['Ready to fly']
+// ]
+// )
