34118e610eaa98861b467686131b725fa7efaf10
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-11 10:06:23 +0200

    problem solved

---
+++ JS/ObjectsAndClasses/Exercises/10.ClassVehicle.js
@@ -0,0 +1,21 @@
+class Vehicle {
+    constructor(type, model, parts, fuel) {
+        this.type = type;
+        this.model = model;
+        this.parts = {
+            engine: parts.engine,
+            power: parts.power,
+            quality: parts.engine * parts.power,
+        };
+        this.fuel = fuel;
+    }
+    drive(x) {
+        this.fuel -= x;
+    }
+}
+
+// let parts = {engine: 9, power: 500};
+// let vehicle = new Vehicle('l', 'k', parts, 840);
+// vehicle.drive(20);
+// console.log(vehicle.fuel);
+
