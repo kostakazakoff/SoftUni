215c74fef8e181fb5b365774f70911eb4a52b99a
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-15 12:28:23 +0200

    problem solved

---
+++ JS/ObjectsAndClasses/MoreExercises/08.Garage.js
@@ -0,0 +1,28 @@
+function garage(arr) {
+    function parseStr(str) {
+        return str.split(':').join(' -');
+    }
+    Object.entries(
+        arr.reduce((data, str) => {
+            let [garageN, carInfo] = str.split(' - ');
+            if (!data[garageN]) {
+                data[garageN] = [];
+            }
+            data[garageN].push(`--- ${parseStr(carInfo)}`);
+            return data;
+        }, {})
+    ).forEach(([k, v]) => {
+        console.log(`Garage № ${k}`);
+        console.log(v.join('\n'))
+    })
+    
+}
+
+// garage(
+//     [
+//         '1 - color: blue, fuel type: diesel',
+//         '1 - color: red, manufacture: Audi',
+//         '2 - fuel type: petrol',
+//         '4 - color: dark blue, fuel type: diesel, manufacture: Fiat'
+//     ]
+// )
