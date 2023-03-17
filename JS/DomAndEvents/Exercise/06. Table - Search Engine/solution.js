dd4afae55cb02d067d987157f87bd5104eadaf9b
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-18 01:33:16 +0200

    problem solved

--- JS/DomAndEvents/Exercise/06. Table - Search Engine/solution.js
+++ JS/DomAndEvents/Exercise/06. Table - Search Engine/solution.js
@@ -2,7 +2,11 @@ function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
 
    function onClick() {
-      //   TODO:
-
+      const searchTxt = document.getElementById('searchField');
+      const allFields = Array.from(document.querySelectorAll('tbody td'));
+      allFields.forEach(el => el.parentElement.removeAttribute('class'));
+      allFields.filter(el  => (el.textContent.toLowerCase().includes(searchTxt.value.toLowerCase()) && searchTxt.value !== ''))
+      .forEach(el => el.parentElement.className = 'select');
+      searchTxt.value = '';
    }
 }
