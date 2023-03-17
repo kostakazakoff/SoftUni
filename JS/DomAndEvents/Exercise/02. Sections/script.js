cec32deadc72cc7d94529f4bdbd6b27ea0c54b13
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-17 18:47:34 +0200

    problem solved

--- JS/DomAndEvents/Exercise/02. Sections/script.js
+++ JS/DomAndEvents/Exercise/02. Sections/script.js
@@ -1,3 +1,14 @@
 function create(words) {
-   console.log('TODO:...');
+   const content = document.getElementById('content')
+   words.forEach(word => createSection(word));
+
+   function createSection(text) {
+      let div = document.createElement('div');
+      let p = document.createElement('p');
+      p.textContent = text;
+      p.style.display = 'none';
+      div.appendChild(p);
+      div.addEventListener('click', () => p.style.display = 'block');
+      content.appendChild(div);
+   }
 }
