20177d700c3564c77410648f6680545f8614599a
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-18 00:39:43 +0200

    problem solved

--- JS/DomAndEvents/Exercise/05. Fill-Dropdown/dropdown.js
+++ JS/DomAndEvents/Exercise/05. Fill-Dropdown/dropdown.js
@@ -1,3 +1,10 @@
 function addItem() {
-    console.log('TODO:...');
+    const textInput = document.getElementById('newItemText');
+    const valueInput = document.getElementById('newItemValue');
+    let newOption = document.createElement('option');
+    newOption.textContent = textInput.value;
+    newOption.value = valueInput.value;
+    document.getElementById('menu').appendChild(newOption);
+    textInput.value = '';
+    valueInput.value = '';
 }
