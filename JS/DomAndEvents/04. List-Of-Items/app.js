752a9e4cc744a9ed468f3234a9adb787b8150f8d
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-17 16:08:38 +0200

    problem solved

--- JS/DomAndEvents/04. List-Of-Items/app.js
+++ JS/DomAndEvents/04. List-Of-Items/app.js
@@ -1,6 +1,7 @@
 function addItem() {
-    const inputText = document.getElementById('newItemText').value;
-    let newItem = document.createElement('li');
-    newItem.textContent = inputText;
+    const inputText = document.getElementById('newItemText');
+    const newItem = document.createElement('li');
+    newItem.textContent = inputText.value;
     document.getElementById('items').appendChild(newItem);
+    inputText.value = '';
 }
--- JS/DomAndEvents/05. Delete-From-Table/app.js
+++ JS/DomAndEvents/05. Delete-From-Table/app.js
@@ -1,3 +1,12 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const inputField = document.querySelector('input[name="email"]');
+    const resultField = document.getElementById('result');
+    const tds = Array.from(document.querySelectorAll("td:nth-child(2)"));
+    let foundTd = tds.find(el => el.textContent === inputField.value);
+    if (foundTd) {
+        foundTd.parentElement.remove();
+        resultField.textContent = 'Deleted.';
+    } else {
+        resultField.textContent = 'Not found.'
+    }
 }
