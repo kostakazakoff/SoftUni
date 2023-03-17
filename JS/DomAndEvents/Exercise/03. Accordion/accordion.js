f60f00539e87dc832c51d7ac51aedf96fd7eb305
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-17 23:36:44 +0200

    problem solved

--- JS/DomAndEvents/Exercise/03. Accordion/accordion.js
+++ JS/DomAndEvents/Exercise/03. Accordion/accordion.js
@@ -1,3 +1,11 @@
 function toggle() {
-    console.log('TODO:...');
+    const btn = document.getElementsByClassName('button')[0];
+    const div = document.getElementById('extra');
+    if (btn.textContent === 'More') {
+        div.style.display = 'block';
+        btn.textContent = 'Less';
+    } else {
+        div.style.display = 'none';
+        btn.textContent = 'More';
+    }  
 }
