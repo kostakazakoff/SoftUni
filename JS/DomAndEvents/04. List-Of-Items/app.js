06bc9a8cb8621f6a721b8e4f733d402401848c1a
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-17 15:04:22 +0200

    problem solved

--- JS/DomAndEvents/04. List-Of-Items/app.js
+++ JS/DomAndEvents/04. List-Of-Items/app.js
@@ -1,3 +1,6 @@
 function addItem() {
-    console.log('TODO:...');
+    const inputText = document.getElementById('newItemText').value;
+    let newItem = document.createElement('li');
+    newItem.textContent = inputText;
+    document.getElementById('items').appendChild(newItem);
 }
