1e7eb7d0eef8b37c5264519f323089cd096be613
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-18 00:20:53 +0200

    problem solved

--- JS/DomAndEvents/Exercise/04. Locked-Profile/app.js
+++ JS/DomAndEvents/Exercise/04. Locked-Profile/app.js
@@ -1,3 +1,18 @@
 function lockedProfile() {
-    console.log('TODO...')
+    Array.from(document.querySelectorAll('button'))
+    .forEach(btn => {
+        btn.addEventListener('click', toggleVisibility);
+    })
+    
+    function toggleVisibility() {
+        const hiddenContent = this.parentElement.querySelector('div');
+        const radioBtnLock = this.parentElement.querySelectorAll('input[type="radio"]')[0];
+        if (this.textContent === 'Show more' && !radioBtnLock.checked) {
+            hiddenContent.style.display = 'block';
+            this.textContent = 'Hide it';
+        } else if (this.textContent === 'Hide it' && !radioBtnLock.checked) {
+            hiddenContent.style.display = 'none';
+            this.textContent = 'Show more';
+        }
+    }
 }
