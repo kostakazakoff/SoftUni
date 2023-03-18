1ccb2e56d62ee61d4d57eac034d3413ffbb52af8
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-18 10:36:05 +0200

    problem solved

--- JS/DomAndEvents/Exercise/07. Format the Text/solution.js
+++ JS/DomAndEvents/Exercise/07. Format the Text/solution.js
@@ -1,3 +1,12 @@
 function solve() {
-  //TODO
+  const outputDiv = document.getElementById('output');
+  const unformatedText = document.getElementById('input');
+  let cleanedText = unformatedText.value.split('.').filter(el => (el.length > 0 && el !== ' '));
+  console.log(cleanedText);
+  while(cleanedText.length > 0) {
+    let fragment = cleanedText.splice(0, 3).join('. ');
+    let newP = document.createElement('p');
+    newP.textContent = `${fragment}.`;
+    outputDiv.appendChild(newP);
+  }
 }
