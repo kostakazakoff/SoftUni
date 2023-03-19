37c018dd612acab353f7823f8f78c5cacf372a0e
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-19 19:14:43 +0200

    refactoring

--- JS/DomAndEvents/MoreExercises/06. Pascal-or-Camel-Case/solution.js
+++ JS/DomAndEvents/MoreExercises/06. Pascal-or-Camel-Case/solution.js
@@ -2,22 +2,18 @@ function solve() {
   const txt = document.querySelector('#text');
   const outputType = document.querySelector('#naming-convention');
   const output = document.querySelector('#result')
-  const start = {
-    'Camel Case': 1,
-    'Pascal Case': 0
-  }
+  const cases = {'Camel Case': 1, 'Pascal Case': 0}
 
   if (txt && outputType) {
     let words = txt.value.toLowerCase().split(' ');
     
-      if (!start.hasOwnProperty(outputType.value)) {
+      if (!cases.hasOwnProperty(outputType.value)) {
         output.textContent = 'Error!'
       } else {
-        for (let i = start[outputType.value]; i < words.length; i++) {
+        for (let i = cases[outputType.value]; i < words.length; i++) {
           words[i] = `${words[i][0].toUpperCase()}${words[i].slice(1)}`
         }
         output.textContent = words.join('')
       }
-    
   }
 }
