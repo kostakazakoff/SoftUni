4d9d7656e54fc7979d2f341f1e26cf83d5c6198c
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-19 15:28:55 +0200

    problem solved

--- JS/DomAndEvents/MoreExercises/03. Mouse-Gradient/app.js
+++ JS/DomAndEvents/MoreExercises/03. Mouse-Gradient/app.js
@@ -1,3 +1,17 @@
 function attachGradientEvents() {
-    console.log('TODO:...');
+    const gradient = document.getElementById('gradient');
+    const output = document.getElementById('result');
+    gradient.addEventListener('mousemove', detectMousePosition);
+    gradient.addEventListener('mouseout', hideInfo);
+
+    function detectMousePosition(e) {
+        const sizeX = gradient.clientWidth;
+        let mousePositionX = e.offsetX;
+        let positionInPercent = Math.floor(mousePositionX * 100 / sizeX);
+        output.textContent = `${positionInPercent}%`;
+    }
+
+    function hideInfo() {
+        output.textContent = '';
+    }
 }
