c3a3e422f2baca8b921e69720e76c62173813b57
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-19 16:50:12 +0200

    problem solved

--- JS/DomAndEvents/MoreExercises/04. Dynamic-Validation/app.js
+++ JS/DomAndEvents/MoreExercises/04. Dynamic-Validation/app.js
@@ -1,3 +1,13 @@
 function validate() {
-    console.log('TODO:...');
+    const emailInputField = document.getElementById('email');
+    emailInputField.addEventListener('change', validateInput);
+
+    function validateInput() {
+        const valid = emailInputField.value.match(/^[a-z]+\@[a-z]+\.[a-z]+$/g);
+        if (!valid) {
+            emailInputField.setAttribute('class', 'error')
+        } else {
+            emailInputField.removeAttribute('class');
+        }
+    }
 }
