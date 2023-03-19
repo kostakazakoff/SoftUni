e83d0993f5a31e3e5be4c504697460f679115029
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-19 20:09:32 +0200

    problem solved

--- JS/DomAndEvents/MoreExercises/07. Search-in-List/search.js
+++ JS/DomAndEvents/MoreExercises/07. Search-in-List/search.js
@@ -1,3 +1,20 @@
 function search() {
-   // TODO
+   const towns = Array.from(document.querySelectorAll('li'));
+   const searchField = document.querySelector('#searchText')
+   const output = document.querySelector('#result');
+   let count = 0;
+
+   towns.forEach(t => {
+      if (t.textContent.toLowerCase().includes(searchField.value.toLowerCase()) && searchField.value.length !== 0) {
+         t.style.textDecoration = 'underline';
+         t.style.fontWeight = 'bold';
+         count++;
+      } else {
+         t.style.textDecoration = 'none';
+         t.style.fontWeight = 'normal';
+      }
+   });
+   output.textContent = `${count} matches found`
+   searchField.value = '';
+   count = 0;
 }
