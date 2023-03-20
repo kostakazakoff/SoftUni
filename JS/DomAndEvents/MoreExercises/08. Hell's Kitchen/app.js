6e6186cb2d5975c918066c077c5e32d5b75cb0d5
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-20 18:44:40 +0200

    refactoring

--- JS/DomAndEvents/MoreExercises/08. Hell's Kitchen/app.js
+++ JS/DomAndEvents/MoreExercises/08. Hell's Kitchen/app.js
@@ -6,42 +6,63 @@ function solve() {
 
    function onClick() {
       let restaurants = {};
+
+      // Add restaurant objects to restaurants
       Array.from(JSON.parse(input.value)).forEach(restaurant => {
-         let [brand, workers] = restaurant.split(' - ');
-         workers = workers.split(', ');
          let totalSallary = 0;
          let numberOfWorkers = 0;
-
+         let [brand, workers] = restaurant.split(' - ');
+         workers = workers.split(', ');
+         
+         // Add workers, average sallary and best sallary to the current restaurant
          workers.forEach(workerInfo => {
             let [name, sallary] = workerInfo.split(' ');
             sallary = Number(sallary);
 
+            // if there is not such restaurant, creating it
             if (!restaurants.hasOwnProperty(brand)) {
-               restaurants[brand] = { 'workers': [], 'averageSallary': 0, 'bestSallary': 0 };
+               restaurants[brand] = { 'workers': {}, 'averageSallary': 0, 'bestSallary': 0 };
             }
 
-            restaurants[brand]['workers'].push({ name, sallary });
+            // Add the worker objects (worker: sallary)
+            restaurants[brand]['workers'][name] = sallary;
             totalSallary += sallary;
+
+            // Select the best sallary
             if (sallary > restaurants[brand]['bestSallary']) {
                restaurants[brand]['bestSallary'] = sallary;
             }
+
             numberOfWorkers++;
          })
-
+         
+         // Calc the average sallary
          restaurants[brand]['averageSallary'] = totalSallary / numberOfWorkers;
-         restaurants[brand]['workers'].sort((a, b) => b['sallary'] - a['sallary']);
       })
 
+      // Sort restaurants by average sallary
       let sotrtedRestBrands = Object.keys(restaurants).sort((a, b) => restaurants[b]['averageSallary'] - restaurants[a]['averageSallary']);
+
+      // Extract the best restaurant by average sallary
       let bestRestBrand = sotrtedRestBrands[0];
+
+      // Formatting the sallaries output
       let averageSallary = restaurants[bestRestBrand]['averageSallary'].toFixed(2)
       let bestSallary = restaurants[bestRestBrand]['bestSallary'].toFixed(2)
+
+      //Adding the output to the field
       bestRestaurantField.textContent = `Name: ${bestRestBrand} Average Salary: ${averageSallary} Best Salary: ${bestSallary}`
 
+      // Creating a best restaurant workers list
       let bestWorkers = [];
-      Object.values(restaurants[bestRestBrand]['workers']).forEach(w => {
-         bestWorkers.push(`Name: ${w['name']} With Salary: ${w['sallary']}`)
+      Object.entries(restaurants[bestRestBrand]['workers']).forEach(([k, v]) => {
+         bestWorkers.push([k, v])
       })
-      bestWorkersField.textContent = bestWorkers.join(' ')
+
+      // Sorting best workers by sallary and adding it to the output field
+      bestWorkersField.textContent = bestWorkers
+      .sort((a, b) => b[1] - a[1])
+      .map(([a, b]) => `Name: ${a} With Salary: ${b}`)
+      .join(' ')
    }
 }
