e7f62b9d34c682e6779671fe7f93f2e01a211468
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-22 14:24:35 +0200

    problem solved

--- JS/HTTPandREST/06. Github-Commits/app.js
+++ JS/HTTPandREST/06. Github-Commits/app.js
@@ -1,4 +1,36 @@
 function loadCommits() {
-    // Try it with Fetch API
-    console.log('TODO...');
+    const usernameInput = document.getElementById('username');
+    const repoInput = document.getElementById('repo');
+    const outputField = document.getElementById('commits')
+    const TARGET_URL = `https://api.github.com/repos/${usernameInput.value}/${repoInput.value}/commits`
+
+    fetch(TARGET_URL, { method: 'GET' })
+        .then((response) => checkError(response))
+        .then((response) => response.json())
+        .then((data) => createOutput(data))
+        .catch((error) => createOutput(error))
+
+    function checkError(response) {
+        if (response.status >= 200 && response.status < 300) {
+            return response;
+        } else {
+            throw Error(response.status);
+        }
+    }
+
+    function createOutput(data) {
+        outputField.textContent = '';
+        if (data.length > 0) {
+            Object.values(data).forEach(v => {
+                const li = document.createElement('li');
+                li.textContent = `${v['commit']['author']['name']}: ${v['commit']['message']}`;
+                outputField.appendChild(li)
+            });
+        } else {
+            const li = document.createElement('li');
+                li.textContent = `${data} (Not Found)`;
+                outputField.appendChild(li)
+        }
+        
+    }
 }
