6cbede7a847da71579d532cdb68f8f41fa70a762
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-22 14:38:02 +0200

    refactoring

--- JS/HTTPandREST/06. Github-Commits/app.js
+++ JS/HTTPandREST/06. Github-Commits/app.js
@@ -2,9 +2,11 @@ function loadCommits() {
     const usernameInput = document.getElementById('username');
     const repoInput = document.getElementById('repo');
     const outputField = document.getElementById('commits')
-    const TARGET_URL = `https://api.github.com/repos/${usernameInput.value}/${repoInput.value}/commits`
+    const username = usernameInput.value;
+    const repo = repoInput.value;
+    const TARGET_URL = 'https://api.github.com/repos/'
 
-    fetch(TARGET_URL, { method: 'GET' })
+    fetch(`${TARGET_URL}${username}/${repo}/commits`, { method: 'GET' })
         .then((response) => checkError(response))
         .then((response) => response.json())
         .then((data) => createOutput(data))
@@ -21,16 +23,15 @@ function loadCommits() {
     function createOutput(data) {
         outputField.textContent = '';
         if (data.length > 0) {
-            Object.values(data).forEach(v => {
+            data.forEach(({ commit }) => {
                 const li = document.createElement('li');
-                li.textContent = `${v['commit']['author']['name']}: ${v['commit']['message']}`;
-                outputField.appendChild(li)
+                li.textContent = `${commit.author.name}: ${commit.message}`;
+                outputField.appendChild(li);
             });
         } else {
             const li = document.createElement('li');
-                li.textContent = `${data} (Not Found)`;
-                outputField.appendChild(li)
+            li.textContent = `${data} (Not Found)`;
+            outputField.appendChild(li);
         }
-        
     }
 }
