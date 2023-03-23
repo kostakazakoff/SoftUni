9b1cf9e7eab4eccbf474e004f9108b0865610253
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-23 16:53:33 +0200

    new tasks

---
+++ JS/ExamPreparation/01/01. The-Pianist/pianist.js
@@ -0,0 +1,21 @@
+function pianist(arr) {
+    const numberOfPieces = Number(arr.shift());
+    
+}
+
+
+
+pianist(
+    [
+        '3',
+        'Fur Elise|Beethoven|A Minor',
+        'Moonlight Sonata|Beethoven|C# Minor',
+        'Clair de Lune|Debussy|C# Minor',
+        'Add|Sonata No.2|Chopin|B Minor',
+        'Add|Hungarian Rhapsody No.2|Liszt|C# Minor',
+        'Add|Fur Elise|Beethoven|C# Minor',
+        'Remove|Clair de Lune',
+        'ChangeKey|Moonlight Sonata|C# Major',
+        'Stop'
+    ]
+)
