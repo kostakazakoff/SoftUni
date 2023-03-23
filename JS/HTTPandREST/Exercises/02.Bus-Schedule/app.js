c2c0abe2974251edf72f86807d38b3a4d4d960bb
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-23 09:35:36 +0200

    refactoring

--- JS/HTTPandREST/Exercises/02.Bus-Schedule/app.js
+++ JS/HTTPandREST/Exercises/02.Bus-Schedule/app.js
@@ -1,10 +1,35 @@
 function solve() {
+    const btnDepart = document.getElementById('depart');
+    const btnArrive = document.getElementById('arrive');
+    const info = document.getElementById('info');
+    const URL = 'http://localhost:3030/jsonstore/bus/schedule/';
+    let stopName;
+    let nextId = '1567';
+    let atWork = true;
+
     function depart() {
-        // TODO: 
+        btnDepart.disabled = true;
+        btnArrive.disabled = false;
+        fetch(`${URL}${nextId}`)
+            .then((response) => response.json())
+            .then((data) => {
+                stopName = data.name;
+                nextId = data.next;
+                info.textContent = `Next stop ${stopName}`;
+            })
+            .catch(() => {
+                btnArrive.disabled = true;
+                info.textContent = 'Error'
+                atWork = false;
+            })
     }
 
     async function arrive() {
-        // TODO:
+        if (atWork) {
+            btnArrive.disabled = true;
+            btnDepart.disabled = false;
+            info.textContent = `Arriving at ${stopName}`;
+        }
     }
 
     return {
