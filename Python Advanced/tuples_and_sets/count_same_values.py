e23aaf75dc226ee2811cd2e3a897e3a131a477d9
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-23 11:15:26 +0200

    optimising

--- Python Advanced/tuples_and_sets/count_same_values.py
+++ Python Advanced/tuples_and_sets/count_same_values.py
@@ -1,12 +1,9 @@
 values = tuple(map(float, input().split()))
 output = {}
-unique = []
 
-for number in values:
-    if number not in unique:
-        unique.append(number)
+for n in values:
+    if n not in output:
+        output[n] = 0
+    output[n] += 1
 
-for number in unique:
-    output[number] = str(f'- {values.count(number)} times')
-
-[print(key, value) for key, value in output.items()]
+[print(key, f' - {value} times') for key, value in output.items()]
