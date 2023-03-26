011f080f2d80e9dd5f6492bbcd01ecfda56b4140
Kosta Kazakov <kostakazakoff@gmail.com>
2023-03-26 14:48:11 +0300

    optimising

--- JS/HTTPandREST/Exercises/04.Blog/app.js
+++ JS/HTTPandREST/Exercises/04.Blog/app.js
@@ -5,28 +5,29 @@ function attachEvents() {
     const postTitle = document.getElementById('post-title');
     const postBody = document.getElementById('post-body');
     const postComments = document.getElementById('post-comments');
+    let caschedPosts = {};
+    let caschedPostComments = {};
 
     const POSTS_URL = 'http://localhost:3030/jsonstore/blog/posts/'
     const POSTS_COMMENTS_URL = 'http://localhost:3030/jsonstore/blog/comments/'
 
     btnLoadPosts.addEventListener('click', () => loadPosts())
-
-    btnViewPosts.addEventListener('click', () => {
-        fetch(`${POSTS_URL}${menu.value}`)
-            .then(res => res.json())
-            .then(post => handlePost(post))
-            .catch(err => console.log(err));
-    })
+    btnViewPosts.addEventListener('click', () => handlePost())
 
     function loadPosts() {
+        while (menu.firstChild) {menu.removeChild(menu.lastChild)};
         fetch(POSTS_URL)
             .then(res => res.json())
-            .then(posts => createMenu(posts))
+            .then((posts) => {caschedPosts = posts; createMenu()})
             .catch(err => console.error(err));
+        fetch(`${POSTS_COMMENTS_URL}`)
+            .then(res => res.json())
+            .then(data => caschedPostComments = data)
+            .catch(err => console.log(err));
     }
 
-    function createMenu(posts) {
-        Object.values(posts).forEach(post => {
+    function createMenu() {
+        Object.values(caschedPosts).forEach(post => {
             const option = document.createElement('option');
             option.value = post.id
             option.textContent = post.title
@@ -34,23 +35,21 @@ function attachEvents() {
         })
     }
 
-    function handlePost(post) {
-        postTitle.textContent = post.title;
-        postBody.textContent = post.body;
-        postComments.textContent = ''
-        fetch(`${POSTS_COMMENTS_URL}`)
-            .then(res => res.json())
-            .then(info => {
-                Object.values(info).forEach(comment => {
-                    if (comment.postId === post.id) {
-                        let li = document.createElement('li');
-                        li.id = comment.id;
-                        li.textContent = comment.text;
-                        postComments.appendChild(li)
-                    }
-                })
-            })
-            .catch(err => console.log(err));
+    function handlePost() {
+        console.log(caschedPosts);
+        let id = menu.value
+        postTitle.textContent = caschedPosts[id]['title'];
+        postBody.textContent = caschedPosts[id]['body'];
+        postComments.textContent = '';
+
+        Object.values(caschedPostComments).forEach(post => {
+            if (post.postId === id) {
+                let li = document.createElement('li');
+                li.id = id;
+                li.textContent = post['text'];
+                postComments.appendChild(li)
+            }
+        })
     }
 }
 
