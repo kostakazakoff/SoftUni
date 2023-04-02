9709e788d1ccffdedc3a22c79717e1ff1935ad09
Kosta Kazakov <kostakazakoff@gmail.com>
2023-04-02 17:32:22 +0300

    problem solved

--- JS/ExamPreparation/03/01. Ski lift/app.js
+++ JS/ExamPreparation/03/01. Ski lift/app.js
@@ -2,44 +2,85 @@ window.addEventListener('load', solve);
 
 function solve() {
     const inputDomSelectors = {
-        'fNameInput': document.getElementById('first-name'),
-        'lNameInput': document.getElementById('last-name'),
-        'peopleInput': document.getElementById('people-count'),
-        'dateInput': document.getElementById('from-date'),
-        'daysInput': document.getElementById('days-count')
+        fName: document.getElementById('first-name'),
+        lName: document.getElementById('last-name'),
+        people: document.getElementById('people-count'),
+        date: document.getElementById('from-date'),
+        days: document.getElementById('days-count')
     }
     const nextBtn = document.getElementById('next-btn');
     const firstForm = document.querySelector('#append-ticket form');
     const ticketPreviewList = document.querySelector('.ticket-info-list');
     const confirmTicketList = document.querySelector('.confirm-ticket');
-    let ticketCasch = [];
+    let ticketCasch = {};
 
     nextBtn.addEventListener('click', toTicketPreview);
 
     function toTicketPreview(e) {
-        e?.preventDefault();
+        if (e) { e.preventDefault() };
 
         const inputIsValid = Object.values(inputDomSelectors).every(field => field.value.length > 0);
-        
-        
-        console.log(ticketCasch);
         if (!inputIsValid) { return };
+
+        const { fName, lName, people, date, days } = inputDomSelectors;
+        ticketCasch = {
+            fName: fName.value,
+            lName: lName.value,
+            people: people.value,
+            date: date.value,
+            days: days.value
+        }
 
         const li = createHTMLElement('li', ticketPreviewList);
         const article = createHTMLElement('article', li);
-        createHTMLElement('h3', article, `Name: ${inputDomSelectors.fNameInput.value} ${inputDomSelectors.lNameInput.value}`);
-        createHTMLElement('p', article, `From date: ${inputDomSelectors.dateInput.value}`);
-        createHTMLElement('p', article, `For ${inputDomSelectors.daysInput.value} days`);
-        createHTMLElement('p', article, `For ${inputDomSelectors.peopleInput.value} people`);
+        createHTMLElement('h3', article, `Name: ${inputDomSelectors.fName.value} ${inputDomSelectors.lName.value}`);
+        createHTMLElement('p', article, `From date: ${inputDomSelectors.date.value}`);
+        createHTMLElement('p', article, `For ${inputDomSelectors.days.value} days`);
+        createHTMLElement('p', article, `For ${inputDomSelectors.people.value} people`);
+
         const editBtn = createHTMLElement('button', li, 'Edit', ['edit-btn']);
         const continueBtn = createHTMLElement('button', li, 'Continue', ['continue-btn']);
 
         editBtn.addEventListener('click', editTask);
         continueBtn.addEventListener('click', toConfirmForm);
+
+        firstForm.reset();
+        nextBtn.disabled = true;
     }
 
     function editTask() {
+        this.parentNode.remove();
+        for (const key in ticketCasch) {
+            inputDomSelectors[key].value = ticketCasch[key];
+        }
+        nextBtn.disabled = false;
+    }
 
+    function toConfirmForm() {
+        const li = createHTMLElement('li', confirmTicketList);
+        li.appendChild(this.parentNode.firstChild);
+
+        const confirmBtn = createHTMLElement('button', li, 'Confirm', ['confirm-btn']);
+        const cancelBtn = createHTMLElement('button', li, 'Cancel', ['cancel-btn']);
+
+        this.parentNode.remove();
+
+        confirmBtn.addEventListener('click', confirm);
+        cancelBtn.addEventListener('click', cancel);
+    }
+
+    function confirm() {
+        const bodyContainer = document.getElementById('body');
+        bodyContainer.innerHTML = '';
+        createHTMLElement('h1', bodyContainer, 'Thank you, have a nice day!', null, 'thank-you');
+        const backBtn = createHTMLElement('button', bodyContainer, 'Back', null, 'back-btn');
+
+        backBtn.addEventListener('click', () => window.location.reload());
+    }
+
+    function cancel() {
+        this.parentNode.innerHTML = '';
+        nextBtn.disabled = false;
     }
 
     function createHTMLElement(type, parentNode, content, classes, id, attributes, useInnerHtml) {
