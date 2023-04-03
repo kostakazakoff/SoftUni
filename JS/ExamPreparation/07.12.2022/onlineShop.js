097ee502a48b910eb0c9d7b43d25108b6ec8d423
Kosta Kazakov <kostakazakoff@gmail.com>
2023-04-03 11:44:21 +0300

    new task

---
+++ JS/ExamPreparation/07.12.2022/onlineShop.js
@@ -0,0 +1,23 @@
+class Warehouse {
+    constructor(warehouseSpace) {
+        this.warehouseSpace = warehouseSpace;
+        this.products = [];
+        this.sales = [];
+    }
+
+    loadingStore(product, quantity, spaceRequired) {
+        // TODO
+    }
+
+    quantityCheck(product, minimalQuantity) {
+        // TODO
+    }
+
+    sellProduct(product) {
+        // TODO
+    }
+
+    revision() {
+        // TODO
+    }
+}
