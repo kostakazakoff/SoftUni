ae8b46b2de4e742dc2b46e00bd9811912d5c78b3
Kosta Kazakov <kostakazakoff@gmail.com>
2023-05-28 22:19:56 +0300

    Edit STATICFILES_DIRS dir

--- Python Web Workshop/pets/views.py
+++ Python Web Workshop/pets/views.py
@@ -3,11 +3,11 @@ from django.shortcuts import render
 def add_pet(request):
     return render(request, 'pets/pet-add-page.html')
 
-def delete_pet(request):
+def delete_pet(request, username, pet_name):
     return render(request, 'pets/pet-delete-page.html')
 
-def details_pet(request):
+def details_pet(request, username, pet_name):
     return render(request, 'pets/pet-details-page.html')
 
-def edit_pet(request):
+def edit_pet(request, username, pet_name):
     return render(request, 'pets/pet-edit-page.html')
