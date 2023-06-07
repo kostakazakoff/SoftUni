a011dd69f9aff6dafb5fe7742b63f100f0d82e4c
Kosta Kazakov <kostakazakoff@gmail.com>
2023-06-07 19:47:47 +0300

    creating postgresql database, migrate

--- Python Web Workshop/pets/models.py
+++ Python Web Workshop/pets/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.utils.text import slugify
 
-# Create your models here.
+'''
+Name - it should consist of a maximum of 30 characters.
+• Personal Pet Photo - the user can link a picture using a URL
+The field date of birth is optional:
+• Date of Birth - pet's day, month, and year of birth
+Slug - a slug automatically generated using the pet's name and the pet's id, separated by a "-" (dash).
+'''
+
+class Pet(models.Model):
+    name = models.CharField(
+        max_length=30,
+        )
+    personal_photo = models.URLField()
+    date_of_birth = models.DateField(
+        null=True,
+        blank=True,
+    )
+    slug = models.SlugField(
+        unique=True,
+    )
+
+    # Auto generate slug
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        if not self.slug:
+            self.slug = slugify(f'{self.name}-{self.id}')
+
+        return super().save(*args, **kwargs)
--- Python Web Workshop/petstagram/settings.py
+++ Python Web Workshop/petstagram/settings.py
@@ -80,12 +80,15 @@ WSGI_APPLICATION = 'petstagram.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "petstagram-database",
+        "USER": "root",
+        "PASSWORD": "root",
+        "HOST": "127.0.0.1",
+        "PORT": "5454",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
