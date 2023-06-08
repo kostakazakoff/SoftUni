b9b3418ce740d3e871d5dfa2655eab6dfef7f561
Kosta Kazakov <kostakazakoff@gmail.com>
2023-06-08 21:50:29 +0300

    created share functionality

--- Python Web Workshop/common/urls.py
+++ Python Web Workshop/common/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from common.views import home_view, like_functionality
+from common.views import home_view, like_functionality, copy_link_to_clipboard
+
 
 urlpatterns = [
     path('', home_view, name='home'),
-    path('photo-like/<int:photo_id>', like_functionality, name='photo like')
+    path('photo-like/<int:photo_id>', like_functionality, name='photo like'),
+    path('photo-share/<int:photo_id>', copy_link_to_clipboard, name='photo share'),
 ]
--- Python Web Workshop/common/views.py
+++ Python Web Workshop/common/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, resolve_url
 from photos.models import Photo
 from .models import PhotoLike
+from pyperclip import copy
+
 
 def home_view(request):
     context = {
@@ -9,15 +11,21 @@ def home_view(request):
     print(context)
     return render(request, 'common/home-page.html', context)
 
+
 def like_functionality(request, photo_id):
     photo = Photo.objects.get(id=photo_id)
-    liked_object = PhotoLike.objects.filter(to_photo_id=photo.id).first()
+    liked_object = PhotoLike.objects.filter(to_photo=photo).first()
 
     if liked_object:
         liked_object.delete()
     else:
         new_liked_object = PhotoLike(to_photo=photo)
         new_liked_object.save()
-    
+
+    return redirect(request.META.get('HTTP_REFERER') + f'#photo-{photo_id}')
+
+
+def copy_link_to_clipboard(request, photo_id):
+    copy(request.META.get('HTTP_HOST') + resolve_url('details photo', photo_id))
 
     return redirect(request.META.get('HTTP_REFERER') + f'#photo-{photo_id}')
