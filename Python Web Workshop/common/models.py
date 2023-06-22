f8681ba628f70b7f0d0df7ae8c6dc7d103c63f25
Kosta Kazakov <kostakazakoff@gmail.com>
2023-06-22 12:49:03 +0300

    create Comment and Like models

--- Python Web Workshop/common/models.py
+++ Python Web Workshop/common/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from photos.models import Photo
 
-# Create your models here.
+
+class Comment(models.Model):
+    COMMENT_MAX_LENGTH = 300
+
+    comment_text = models.TextField(
+        max_length=COMMENT_MAX_LENGTH,
+        null=False,
+        blank=False,
+    )
+    date_of_publication = models.DateTimeField(
+        auto_now_add=True,
+    )
+    to_photo = models.ForeignKey(
+        Photo,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        related_name='photo_comments',
+    )
+
+
+class Like(models.Model):
+    to_photo = models.ForeignKey(
+        Photo,
+        null=False,
+        blank=False,
+        on_delete=models.CASCADE,
+        related_name='photo_likes',
+    )
