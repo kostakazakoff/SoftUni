f86525b2a451b5b851c6f5cf2ae24cc88891d49b
Kosta Kazakov <kostakazakoff@gmail.com>
2023-06-23 12:34:06 +0300

    working on

--- Python Web Workshop/common/views.py
+++ Python Web Workshop/common/views.py
@@ -3,6 +3,10 @@ from .models import Photo, Like
 from pyperclip import copy
 
 
+def to_url(request, id):
+    return request.META['HTTP_REFERER'] + f'#photo-{id}'
+
+
 def home(request):
     photos = Photo.objects.all()
     context = {'photos': photos}
@@ -14,11 +18,9 @@ def like_functionality(request, id):
     likes = Like.objects.filter(to_photo_id=id)
     if likes.count() > 1:
         [like.delete() for like in likes]
-
-
-    return redirect(request.META['HTTP_REFERER'] + f'#photo-{id}')
+    return redirect(to_url(request, id))
 
 
 def share_functionality(request, id):
     copy(request.META['HTTP_HOST'] + resolve_url('photo details', id))
-    return redirect(request.META['HTTP_REFERER'] + f'#photo-{id}')
+    return redirect(to_url(request, id))
