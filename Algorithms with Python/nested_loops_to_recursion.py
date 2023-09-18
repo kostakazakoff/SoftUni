e3e906376b3192e2b4593128315c15f20570e75a
Kosta Kazakov <kostakazakoff@gmail.com>
2023-09-18 17:34:37 +0300

    problem solved

--- Algorithms with Python/nested_loops_to_recursion.py
+++ Algorithms with Python/nested_loops_to_recursion.py
@@ -1,2 +1,12 @@
-def print_loops(n):
-    
+def print_loops(idx, n, vector):
+    if idx == n:
+        print(*vector)
+        return
+    for i in range(n):
+        vector[idx] = i + 1
+        print_loops(idx+1, n, vector)
+
+
+loops = int(input())
+vector = [None] * loops
+print_loops(0, loops, vector)
