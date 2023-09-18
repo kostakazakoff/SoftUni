25e42a34c947227aa1d584db79de463a07ee244a
Kosta Kazakov <kostakazakoff@gmail.com>
2023-09-18 15:26:55 +0300

    problem var2 solved

--- Algorithms with Python/reverse_array.py
+++ Algorithms with Python/reverse_array.py
@@ -5,5 +5,18 @@ def reverse_arr(arr, idx = 0):
     arr[idx], arr[len(arr) - idx - 1] = arr[len(arr) - idx - 1], arr[idx]
     reverse_arr(arr, idx + 1)
 
+
 array = input().split(' ')
 reverse_arr(array)
+
+
+# def reverse_arr(arr, count, reversed_arr):
+#     if count == len(arr):
+#         print(*reversed_arr, sep=' ')
+#         return
+#     reversed_arr.append(arr[-count - 1])
+#     reverse_arr(arr, count + 1, reversed_arr)
+
+
+# array = input().split(' ')
+# reverse_arr(array, 0, [])
