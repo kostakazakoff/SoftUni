a5e1f8f8aa182ff1b62f18ba6b19ad485720ef2d
Kosta Kazakov <kostakazakoff@gmail.com>
2023-09-18 14:38:00 +0300

    problem solved

--- Algorithms with Python/recursive_fibonacci.py
+++ Algorithms with Python/recursive_fibonacci.py
@@ -1,2 +1,15 @@
-def get_fibonacci(n):
-    pass
+class Fibonacci:
+    def __init__(self):
+        self.casche = {}
+
+    def sequence(self, n):
+        if n <= 1:
+            return 1
+        if n not in self.casche:
+            self.casche[n] = self.sequence(n-1) + self.sequence(n-2)
+        return self.casche[n]
+
+
+n = int(input())
+fibonacci = Fibonacci()
+print(fibonacci.sequence(n))
