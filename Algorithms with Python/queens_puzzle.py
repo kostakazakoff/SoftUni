2650ec9996279096214637c772de3f988fe1d084
Kosta Kazakov <kostakazakoff@gmail.com>
2023-09-18 09:35:01 +0300

    optimising

--- Algorithms with Python/queens_puzzle.py
+++ Algorithms with Python/queens_puzzle.py
@@ -6,6 +6,32 @@ class ChessPad:
         self.used_left_diags = set()
         self.used_right_diags = set()
 
+    def set_queen(self, row, col):
+        self.matrix[row][col] = '*'
+        self.used_rows.add(row)
+        self.used_cols.add(col)
+        self.used_left_diags.add(row-col)
+        self.used_right_diags.add(row+col)
+
+    def remove_queen(self, row, col):
+        self.matrix[row][col] = '-'
+        self.used_rows.remove(row)
+        self.used_cols.remove(col)
+        self.used_left_diags.remove(row-col)
+        self.used_right_diags.remove(row+col)
+
+    def position_is_free(self, row, col):
+        if col < 0 or col >= len(self.matrix[0]):
+            return False
+        if self.matrix[row][col] == '*':
+            return False
+        if row in self.used_rows\
+            or col in self.used_cols\
+            or row - col in self.used_left_diags\
+            or row + col in self.used_right_diags:
+                return False
+        return True
+
 
 matrix = [['-'] * 8 for _ in range(8)]
 gamepad = ChessPad(matrix)
@@ -17,39 +43,10 @@ def put_queens(row):
         print()
         return
     for col in range(8):
-        if is_posible(row, col):
-            set_queen(row, col)
+        if gamepad.position_is_free(row, col):
+            gamepad.set_queen(row, col)
             put_queens(row+1)
-            remove_queen(row, col)
-
-
-def is_posible(row, col):
-    if row < 0 or col < 0 or col >= 8:
-        return False
-    if gamepad.matrix[row][col] == '*':
-        return False
-    if row in gamepad.used_rows\
-        or col in gamepad.used_cols\
-        or row - col in gamepad.used_left_diags\
-        or row + col in gamepad.used_right_diags:
-            return False
-    return True
-    
-
-def set_queen(row, col):
-    gamepad.matrix[row][col] = '*'
-    gamepad.used_rows.add(row)
-    gamepad.used_cols.add(col)
-    gamepad.used_left_diags.add(row-col)
-    gamepad.used_right_diags.add(row+col)
-
-
-def remove_queen(row, col):
-    gamepad.matrix[row][col] = '-'
-    gamepad.used_rows.remove(row)
-    gamepad.used_cols.remove(col)
-    gamepad.used_left_diags.remove(row-col)
-    gamepad.used_right_diags.remove(row+col)
+            gamepad.remove_queen(row, col)
 
 
 put_queens(0)
