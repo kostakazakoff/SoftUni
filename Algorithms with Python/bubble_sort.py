806bd9a6e193bc4137666f52837bb62d77b93145
Kosta Kazakov <kostakazakoff@gmail.com>
2023-09-27 16:36:46 +0300

    optimising

--- Algorithms with Python/bubble_sort.py
+++ Algorithms with Python/bubble_sort.py
@@ -1,14 +1,25 @@
-def bubble_sort(arr):
-    nums_are_sorted = False
-    search_end = len(arr)
-    while not nums_are_sorted:
-        nums_are_sorted = True
-        for idx in range(1, search_end):
-            if arr[idx-1] > arr[idx]:
-                arr[idx-1], arr[idx] = arr[idx], arr[idx-1]
-                nums_are_sorted = False
-        search_end -= 1
+# def bubble_sort(arr):
+#     nums_are_sorted = False
+#     search_end = len(arr)
+
+#     while not nums_are_sorted:
+#         nums_are_sorted = True
+#         for idx in range(1, search_end):
+#             if arr[idx-1] > arr[idx]:
+#                 arr[idx-1], arr[idx] = arr[idx], arr[idx-1]
+#                 nums_are_sorted = False
+#         search_end -= 1
     
+#     print(' '.join(str(n) for n in arr))
+
+
+def bubble_sort(arr):
+    for i in range(len(arr)):
+        end_of_loop = len(arr) - i
+        for j in range(1, end_of_loop):
+            if arr[j] < arr[j-1]:
+                arr[j], arr[j-1] = arr[j-1], arr[j]
+
     print(' '.join(str(n) for n in arr))
 
 
