a5d21efd3e577141503008e352a1647d686a9356
Kosta Kazakov <kostakazakoff@gmail.com>
2023-09-27 16:28:19 +0300

    problem solved

--- Algorithms with Python/bubble_sort.py
+++ Algorithms with Python/bubble_sort.py
@@ -1,5 +1,16 @@
+def bubble_sort(arr):
+    nums_are_sorted = False
+    search_end = len(arr)
+    while not nums_are_sorted:
+        nums_are_sorted = True
+        for idx in range(1, search_end):
+            if arr[idx-1] > arr[idx]:
+                arr[idx-1], arr[idx] = arr[idx], arr[idx-1]
+                nums_are_sorted = False
+        search_end -= 1
+    
+    return ' '.join(str(n) for n in arr)
+
+
 nums = [int(x) for x in input().split()]
-
-
-
-print(nums)
+print(bubble_sort(nums))
