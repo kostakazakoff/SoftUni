41f0645a27c30596ed1b9645cd384bb99e7d999a
Kosta Kazakov <kostakazakoff@gmail.com>
2023-10-27 19:48:01 +0300

    New React app

--- React/my-first-react-app/src/assets/chocoData.jsx
+++
@@ -1,7 +0,0 @@
-const chocoData = [
-    {img: "images/chocolate2.png", type: 'chocolate', price: '5.0'},
-    {img: "images/chocolate3.png", type: 'chocolate', price: '7.0'},
-    {img: "images/chocolate1.png", type: 'chocolate', price: '3.0'},
-]
-
-export default chocoData
--- React/my-first-react-app/src/components/Image1.jsx
+++
@@ -1,7 +0,0 @@
-export default function Image1() {
-    return (
-        <div className="img-box">
-            <img src="images/chocolate1.png" alt="" />
-        </div>
-    )
-}
